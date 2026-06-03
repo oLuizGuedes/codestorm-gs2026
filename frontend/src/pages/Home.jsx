56faadecfa1bf01c6b253fb50ededbd3815bd70a
oLuizGuedes <luizgustavobg09@gmail.com>
2026-06-03 18:26:58 -0300

    Cria estrutura inicial das páginas e componentes

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>AstroSupply</h1>
+      <p>Sistema Inteligente de Monitoramento de Recursos Espaciais</p>
+    </div>
+  );
+}
+
+export default Home;
