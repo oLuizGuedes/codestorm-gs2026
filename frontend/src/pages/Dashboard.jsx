4c11809c88e02fe9abe3eff380305fc2039aee1c
oLuizGuedes <luizgustavobg09@gmail.com>
2026-06-03 19:26:55 -0300

    Adiciona estrutura inicial de estilos e dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,33 +1,17 @@
-
 import ResourceCard from "../components/ResourceCard";
+import "../styles/dashboard.css";
 
 function Dashboard() {
   return (
-    <div>
+    <div className="dashboard">
       <h1>Dashboard AstroSupply</h1>
 
-      <div
-        style={{
-          display: "flex",
-          gap: "20px",
-          flexWrap: "wrap",
-        }}
-      >
+      <div className="dashboard-cards">
         <ResourceCard nome="Oxigênio" porcentagem={85} />
         <ResourceCard nome="Água" porcentagem={72} />
         <ResourceCard nome="Energia" porcentagem={91} />
         <ResourceCard nome="Alimentos" porcentagem={64} />
       </div>
-=======
-function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-
-      <h3>Oxigênio: 85%</h3>
-      <h3>Água: 72%</h3>
-      <h3>Energia: 91%</h3>
-      <h3>Alimentos: 64%</h3>
     </div>
   );
 }
