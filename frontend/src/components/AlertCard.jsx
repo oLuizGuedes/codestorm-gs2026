b9a6447c150d87815fc9f1f6780a08631bf07fec
gustavomrm <gustavo.mrm7@gmail.com>
2026-06-09 13:23:33 -0300

    Adicionando a estilização da pagina alertas

--- frontend/src/components/AlertCard.jsx
+++ frontend/src/components/AlertCard.jsx
@@ -1,10 +1,24 @@
-function AlertCard({ titulo, mensagem }) {
+import AlertCard from "../components/AlertCard";
+import "../styles/alertas.css";
+
+function Alertas() {
   return (
-    <div className="alert-card">
-      <h3>{titulo}</h3>
-      <p>{mensagem}</p>
+    <div className="alertas-container">
+      <h1>Centro de Alertas</h1>
+
+      <div className="alertas-grid">
+        <AlertCard
+          titulo="Estoque baixo"
+          mensagem="Alimentos abaixo de 70%"
+        />
+
+        <AlertCard
+          titulo="Consumo elevado"
+          mensagem="Uso de água acima do esperado"
+        />
+      </div>
     </div>
   );
 }
 
-export default AlertCard;
+export default Alertas;
